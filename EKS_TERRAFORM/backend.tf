51c5e708f156b086be4b7f425a25c3a26dc03d22
cloudmanoj <119481545+cloudmanoj@users.noreply.github.com>
2023-12-31 19:02:04 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cici3112" # Replace with your actual S3 bucket name
+    bucket = "cici311223" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
